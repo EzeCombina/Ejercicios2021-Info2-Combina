f42a360dab0f87f42b1ae1d94f43c3188c0bbfc7
EzeCombina <ezequielcombina1@gmail.com>
2021-03-27 02:39:08 -0300

    Completado

---
+++ Ej1-Promedio.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()  {
+	
+	int i, a;
+	float Numeros[50], Suma=0, Prom=0;
+	
+	printf("Ingrese los numeros de los cuales obtendremos el promedio: \n");
+	printf("Para finalizar escribir: 9999\n");
+	
+	for ( i = 0;; i++)
+	{
+		printf("\n");
+		scanf("%f", &Numeros[i]);
+		
+		if(Numeros[i]==9999)
+		{
+			break;
+		}
+	}
+	
+	a=i-1;
+	
+	for ( i = 0;; i++) 
+	{
+		Suma=Suma + Numeros[i]; 
+		
+		if(i==a)
+		{
+			break;
+		}
+	}
+	
+	a++;
+	
+	Prom=Suma/a;
+	
+	printf("\nEl valor del promedio es: %.3f", Prom);
+	
+	return 0;
+	
+}
