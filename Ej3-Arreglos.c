15f3563ef9379a7c1cdd7dcaba80bf1369477a1f
EzeCombina <ezequielcombina1@gmail.com>
2021-03-30 13:45:34 -0300

    Completado(Se puede mejorar con funciones)

--- Ej3-Arreglos.c
+++ Ej3-Arreglos.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()  {
 
     char Letra;
     int f[11];
-    float b[11];
+    float c[100], R=0;
     int g[5] = { 8, 8, 8, 8, 8}, i;
+    double a[11], b[34];
 
     printf("Ingresar la letra del apartado que quiera ver: ");
     scanf("%c", &Letra);
@@ -24,9 +26,9 @@ int main()  {
         case 'b':
       
         printf("Ingrese el valor: ");
-        scanf("%f", &b[4]);
+        scanf("%lf", &b[4]);
 
-        printf("El valor que se ingreso es: %.4f", b[4]);
+        printf("El valor que se ingreso es: %.4lf", b[4]);
 
         break;
 
@@ -40,9 +42,33 @@ int main()  {
         break;
 
         case 'd':
+
+        for( i = 0; i < 100; i++)
+        {
+            c[i]= rand() % 500;
+            R = R + c[i];
+        }
+
+        printf("El valor de i es: %d\n", i);
+        printf("La sumatoria da:  %.3f ", R);
+
         break;
 
         case 'e':
+
+        printf("Ingrese los valores del primer arreglo (11 valores):\n");
+
+        for( i = 0; i < 11; i++)
+        {
+            scanf("%lf", &a[i]);
+        }
+
+        for( i = 0; i < 11; i++)
+        {
+            b[i] = a[i];
+            printf("El valor %d del arreglo b es: %.3lf\n", i, b[i]);  
+        }
+
         break;
 
     }
