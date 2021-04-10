a62461f5faf77b3316b45dad2d09bcaf779c060e
EzeCombina <ezequielcombina1@gmail.com>
2021-04-10 15:16:13 -0300

    Completado (Se puede mejorar)

--- Ej5-SecuenciaLed.cpp
+++ Ej5-SecuenciaLed.cpp
@@ -27,7 +27,7 @@ void loop()
   
   if(digitalRead(pulsador2))
   {
-     x=1;
+    x=1;
   }
   
   if(x==1)
@@ -38,38 +38,74 @@ void loop()
 
 int derecha()  {
   
-    digitalWrite(12, HIGH);
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);    
-    delay(500);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);    
+  delay(500);
+
+  if(digitalRead(pulsador2))
+  {
+    x=1;
+    return (0);
+  }
     
-    digitalWrite(12, LOW);
-    digitalWrite(11, HIGH);
-    digitalWrite(10, LOW);    
-    delay(500);
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);    
+  delay(500);
+
+  if(digitalRead(pulsador2))
+  {
+    x=1;
+    return (0);
+  }
     
-    digitalWrite(12, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(10, HIGH);    
-    delay(500);  
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, HIGH);    
+  delay(500);
+
+  if(digitalRead(pulsador2))
+  {
+    x=1;
+    return (0);
+  }
   
 }
 
 int izquierda()  {
   
-    digitalWrite(12, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(10, HIGH);    
-    delay(500);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, HIGH);    
+  delay(500);
+
+  if(digitalRead(pulsador1))
+  {
+    x=0;
+    return (0);
+  }
       
-    digitalWrite(12, LOW);
-    digitalWrite(11, HIGH);
-    digitalWrite(10, LOW);    
-    delay(500);
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);    
+  delay(500);
+
+  if(digitalRead(pulsador1))
+  {
+    x=0;
+    return (0);
+  }
     
-    digitalWrite(12, HIGH);
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);    
-    delay(500); 
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);    
+  delay(500); 
+
+  if(digitalRead(pulsador1))
+  {
+    x=0;
+    return (0);
+  }
   
 }
