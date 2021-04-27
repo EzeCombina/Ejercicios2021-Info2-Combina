6910a51ca0870e63dbddd85f02c9517389f17b07
EzeCombina <ezequielcombina1@gmail.com>
2021-04-27 19:48:42 -0300

    Compleado

--- C/Ej7-IntroEstructuras.c
+++ C/Ej7-IntroEstructuras.c
@@ -23,13 +23,13 @@ int main(){
     ptrCliente = &registroCliente;
 
     printf("Ingresar apellido: ");
-    scanf("%s", &registroCliente.apellido);
+    scanf("%s", registroCliente.apellido);
     printf("%s", registroCliente.apellido);
     
     printf("\n%s", ptrCliente->apellido);
 
     printf("\nIngresar nombre: ");
-    scanf("%s", &registroCliente.nombre);
+    scanf("%s", registroCliente.nombre);
     printf("%s", registroCliente.nombre);
 
     printf("\n%s", ptrCliente->nombre);
@@ -41,31 +41,31 @@ int main(){
     printf("\n%d", ptrCliente->numeroCliente);
 
     printf("\nIngresar numero telefonico del cliente: ");
-    scanf("%s", &registroCliente.personal.numeroTelefonico);
+    scanf("%s", registroCliente.personal.numeroTelefonico);
     printf("%s", registroCliente.personal.numeroTelefonico);
 
     printf("\n%s", ptrCliente->personal.numeroTelefonico);
 
     printf("\nIngresar direccion del cliente: ");
-    scanf("%s", &registroCliente.personal.direccion);
+    scanf("%s", registroCliente.personal.direccion);
     printf("%s", registroCliente.personal.direccion);
 
     printf("\n%s", ptrCliente->personal.direccion);
 
     printf("\nIngresar ciudad del cliente: ");
-    scanf("%s", &registroCliente.personal.ciudad);
+    scanf("%s", registroCliente.personal.ciudad);
     printf("%s", registroCliente.personal.ciudad);
 
     printf("\n%s", ptrCliente->personal.ciudad);
 
     printf("\nIngresar estado del cliente: ");
-    scanf("%s", &registroCliente.personal.estado);
+    scanf("%s", registroCliente.personal.estado);
     printf("%s", registroCliente.personal.estado);
 
     printf("\n%s", ptrCliente->personal.estado);
 
     printf("\nIngresar codigo postal del cliente: ");
-    scanf("%s", &registroCliente.personal.codigoPostal);
+    scanf("%s", registroCliente.personal.codigoPostal);
     printf("%s", registroCliente.personal.codigoPostal);
 
     printf("\n%s", ptrCliente->personal.codigoPostal);
