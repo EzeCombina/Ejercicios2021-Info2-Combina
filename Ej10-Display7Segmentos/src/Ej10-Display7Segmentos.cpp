a5adb5698f4eb850cabba04e53ca5352ab6da579
EzeCombina <ezequielcombina1@gmail.com>
2021-04-28 18:11:19 -0300

    Completado

---
+++ Ej10-Display7Segmentos/src/Ej10-Display7Segmentos.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  DDRD = DDRD | B11111111;                        
+}
+
+byte led;
+int i=0;
+
+void loop() {
+  for(led = B00000001; led <=B10000000; led = led<<1)
+  {
+    PORTD = led;
+    delay(1000);    
+  }
+}
