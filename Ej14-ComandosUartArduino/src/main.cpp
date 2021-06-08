e8b0d75f0b3f6c84b88712c6bdad07bc95583c00
EzeCombina <ezequielcombina1@gmail.com>
2021-06-08 18:23:13 -0300

    Pruebas

--- Ej14-ComandosUartArduino/src/main.cpp
+++ Ej14-ComandosUartArduino/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
-  
+#define LED 13
+
+String datosRecibidos;
+
+void setup()
+{
+  Serial.begin(115200);   //Inicializa el puerto  serie a 115200 baudios
+  pinMode(LED,OUTPUT);
 }
 
-void loop() {
-  
+void loop()
+{
+  if(Serial.available()>0)      //Procede a la lectura solo si hay datos disponibles en el buffer
+  {
+    datosRecibidos = Serial.readString();
+
+    Serial.print("OK");
+    Serial.println(datosRecibidos);
+
+    if(datosRecibidos == "E")   //Si el comando recibido es E enciende el led, si es A lo apaga
+    {
+      digitalWrite(LED, HIGH);
+    }
+    else if (datosRecibidos == "A")
+    {
+      digitalWrite(LED, LOW);
+    }
+
+  }
 }
